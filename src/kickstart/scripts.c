2caa4b4455dbf220cec920bb9f4777263d49587a
simon0302010 <simon0302010@gmail.com>
2026-02-20 10:53:34 +0100

    Customizable interpreter for post install script

--- src/kickstart/scripts.c
+++ src/kickstart/scripts.c
@@ -2,6 +2,7 @@
 
 #include "../utils/utils.h"
 #include "../globals.h"
+#include "gtk/gtkdropdown.h"
 
 void new_script_window_with_title(const char *title) {
     g_print("Creating scripts window \"%s\"\n", title);
@@ -22,7 +23,6 @@ void new_script_window_with_title(const char *title) {
     // main part
     GtkWidget *code_label = create_label("Code");
     gtk_widget_set_halign(code_label, GTK_ALIGN_CENTER);
-    //gtk_widget_set_hexpand(code_label, TRUE);
     gtk_grid_attach(GTK_GRID(main_grid), code_label, 0, 0, 1, 1);
 
     GtkWidget *code_scroll = gtk_scrolled_window_new();
@@ -33,9 +33,27 @@ void new_script_window_with_title(const char *title) {
     gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(code_scroll), options.post_install.code);
     gtk_grid_attach(GTK_GRID(main_grid), code_scroll, 0, 1, 1, 1);
 
+    // options
+    GtkWidget *options_label = create_label("Options");
+    gtk_widget_set_halign(options_label, GTK_ALIGN_CENTER);
+    gtk_grid_attach(GTK_GRID(main_grid), options_label, 1, 0, 1, 1);
+
+    GtkWidget *options_grid = gtk_grid_new();
+    gtk_grid_set_row_spacing(GTK_GRID(options_grid), 8);
+    gtk_grid_set_column_spacing(GTK_GRID(options_grid), 10);
+    gtk_widget_set_margin_top(options_grid, 10);
+
+    gtk_grid_attach(GTK_GRID(options_grid), create_label("Interpreter:"), 0, 0, 1, 1);
+    gtk_grid_attach(GTK_GRID(options_grid), options.post_install.interpreter, 1, 0, 1, 1);
+
+    gtk_grid_attach(GTK_GRID(main_grid), options_grid, 1, 1, 1, 1);
+
     gtk_window_present(GTK_WINDOW(window));
 }
 
 void setup_scripts_window_items() {
     options.post_install.code = gtk_text_view_new();
+
+    const char *interpreters[] = {"/usr/bin/sh", "/usr/bin/bash", "/usr/bin/python", NULL};
+    options.post_install.interpreter = gtk_drop_down_new_from_strings(interpreters);
 }
